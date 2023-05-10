00d654882f3182fa59b1e7c0374b235ea21097dc
HongYeSun <kellysun3615@gmail.com>
2023-05-10 20:45:10 +0900

    before pull 0510

--- src/test/java/WeatherWearsYou/external/ChatGptControllerTest.java
+++ src/test/java/WeatherWearsYou/external/ChatGptControllerTest.java
@@ -12,7 +12,7 @@ public class ChatGptControllerTest {
         // Manually create instances of required dependencies
         ObjectMapper jsonMapper = new ObjectMapper();
         OpenAiApiClient client = new OpenAiApiClient(); // You may need to set the API key manually
-        client.setOpenaiApiKey("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"); // Replace with your actual API key
+        client.setOpenaiApiKey("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"); // Replace with your actual API key
 
         WeatherController weatherController = new WeatherController(); // You may need to set the API key manually
 
