13965a20124f8dec5b386c78401fe9d2d00dd775
ejeong0303 <106753410+ejeong0303@users.noreply.github.com>
2023-05-23 13:03:05 +0900

    Update WeatherWearsYouApplication.java

--- src/main/java/WeatherWearsYou/WeatherWearsYouApplication.java
+++ src/main/java/WeatherWearsYou/WeatherWearsYouApplication.java
@@ -18,7 +18,7 @@ public class WeatherWearsYouApplication {
 		return new WebMvcConfigurer(){
 			@Override
 			public void addCorsMappings(CorsRegistry registry){
-				registry.addMapping("/**").allowedOrigins("http://localhost:8080");
+				registry.addMapping("/**").allowedOrigins("http://localhost:5000");
 			}
 		};
 	}
